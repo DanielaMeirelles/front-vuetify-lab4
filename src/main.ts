d3d470e7156f19c9dbbe41eec6231c1ab912b3ea
DanielaMeirelles <daniela.meirelles90@outlook.com>
2025-05-22 20:27:18 -0300

    folder struture and pinia

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,30 @@
-/**
- * main.ts
- *
- * Bootstraps Vuetify and other plugins then mounts the App`
- */
-
-// Plugins
 import { registerPlugins } from '@/plugins'
-
-// Components
-import App from './App.vue'
-
-// Composables
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
+import App from './App.vue'
 import axios from 'axios'
+import router from './router'
+import { loginStore } from './stores/login'
 
 axios.defaults.baseURL = 'https://zany-adventure-4657pjx75wg3j74x-8080.app.github.dev'
 
+axios.interceptors.request.use((config) => {
+  const store = loginStore()
+  if (store.token) {
+    config.headers.Authorization = store.token
+  }
+  return config
+})
+
 const app = createApp(App)
 
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
+app.use(pinia)
+app.use(router)
 registerPlugins(app)
 
 app.mount('#app')
--- src/router/index.ts
+++ src/router/index.ts
@@ -5,31 +5,30 @@
  */
 
 // Composables
-import { createRouter, createWebHistory } from 'vue-router/auto'
-import { routes } from 'vue-router/auto-routes'
+import Home from '@/pages/Home.vue'
+import Login from '@/pages/Login.vue'
+import Register from '@/pages/Register.vue'
+import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes,
-})
-
-// Workaround for https://github.com/vitejs/vite/issues/11804
-router.onError((err, to) => {
-  if (err?.message?.includes?.('Failed to fetch dynamically imported module')) {
-    if (!localStorage.getItem('vuetify:dynamic-reload')) {
-      console.log('Reloading page to fix dynamic import error')
-      localStorage.setItem('vuetify:dynamic-reload', 'true')
-      location.assign(to.fullPath)
-    } else {
-      console.error('Dynamic import error, reloading page did not fix it', err)
-    }
-  } else {
-    console.error(err)
-  }
-})
-
-router.isReady().then(() => {
-  localStorage.removeItem('vuetify:dynamic-reload')
+  routes: [
+  {
+    path: '/',
+    name: 'Login',
+    component: Login,
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register,
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: Home,
+  },
+]
 })
 
 export default router
